7f344bf0fb819cdec4b2815cbf4469eca075e54d
Nidhi8595 <155657850+Nidhi8595@users.noreply.github.com>
2024-08-15 18:16:31 +0530

    Data Types

---
+++ Data_Types.js
@@ -0,0 +1,20 @@
+"use strict"; // using newer version
+
+let name = "Nidhi"
+let age = 18
+let isLoggedIn = false
+
+let state; // undefined
+
+// number -> 2 to power 53
+// bigint -> for higher range
+// string -> ""
+// boolean -> true /false
+// null -> standalone value / object
+// undefined -> data type
+// symbol -> unique
+
+// object { keys and values}
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
