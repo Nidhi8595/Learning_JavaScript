2f55965f8d8cad24b6d419553bfd721bf5a042c8
Nidhi8595 <155657850+Nidhi8595@users.noreply.github.com>
2024-09-27 14:45:56 +0530

    Modify Text When Button is Clicked

---
+++ Modify Text/script.js
@@ -0,0 +1,10 @@
+
+var btn = document.querySelector("button");
+var par = document.querySelector("p");
+
+
+btn.addEventListener("click", function () {
+
+    par.textContent = "Button clicked & text modified!"
+
+})
